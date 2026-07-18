4e1deb4f95c7c5787a5d11bf23c8c4089bf07741
Parham Afshari <parhamafshari666@gmail.com>
2026-07-18 15:36:01 +0330

    Made the project ready for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
+const API_URL = process.env.API_URL || "http://localhost:4000";
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:4000/api/:path*",
+        destination: `${API_URL}/api/:path*`,
       },
       {
         source: "/avatars/:path*",
-        destination: "http://localhost:4000/avatars/:path*",
+        destination: `${API_URL}/avatars/:path*`,
       },
     ];
   },
